48f14d6773973acfa5d6b3a2f87861bc308993fc
jonty1231 <jontythakur12@gmail.com>
2025-08-27 03:58:38 +0530

    first commit

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 const pool = mysql.createPool({
     host:"localhost",
-      user: "root",       
-  password: "",
-  database: "nodejsdb",
+      user: process.env.user,       
+  password: process.env.password,
+  database: process.env.database,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
