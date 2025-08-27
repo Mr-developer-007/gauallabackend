6850fe4e33170c5d65c359710129e0664ae3a003
jonty1231 <jontythakur12@gmail.com>
2025-08-27 10:41:50 +0530

    done

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ dotenv.config()
 
 
 const pool = mysql.createPool({
-    host:"localhost",
-      user: process.env.USER,       
-  // password: "",
-  password: process.env.password,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0, 
-})
+  queueLimit: 0,
+});
 
 export default pool;
